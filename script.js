753763e916d635d4b34a977da29c06b05225a8d1
rahimabeer345-droid <rahimabeer345@gmail.com>
2025-12-31 13:33:43 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,187 +1,151 @@
-function date(params) {
-    var now =new Date();
-    alert(now);
-}
-
-function names(firstname,lastname) {
-    var fullname= firstname + " " + lastname;
-    alert(fullname);
-}
-names("Abeer","Ahmed");
-
-
-function addNumbers() {
-    var num1 = prompt("Enter first number");
-    var num2 = prompt("Enter second number");
-    return num1 + num2;
-}
-
-alert("Sum is: " + addNumbers());
-
-
-function calculator(num1, num2, operator) {
-    var result;
-
-    if (operator === "+") {
-        result = num1 + num2;
-    } 
-    else if (operator === "-") {
-        result = num1 - num2;
-    } 
-    else if (operator === "*") {
-        result = num1 * num2;
-    } 
-    else if (operator === "/") {
-        result = num1 / num2;
-    } 
-    else {
-        result = "Invalid operator";
+function power(a,b) {
+    var result = 1
+    for (let i = 1; i <=b; i++) {
+        result = result*a;
     }
-
     return result;
 }
-
-var n1 = prompt("Enter first number");
-var n2 = prompt("Enter second number");
-var op = prompt("Enter operator (+, -, *, /)");
-
-alert("Calculator Result: " + calculator(n1, n2, op));
+alert(power(2,3));
 
 
-
-function square(num) {
-    return num * num;
-}
-
-var number = prompt("Enter a number to find square");
-alert("Square is: " + square(number));
-
-
-function factorial(num) {
-    var result = 1;
-for (let i = 1; i <=num; i++) {
-    result = result *i;
-}
-    return result;
-}
-var number = prompt("Enter a number")
-alert("factorial is" + factorial(number));
-
-function sc(start, end) {
-    var result = "";
-
-    for (let i = start; i<end; i++) {
-        result = result + i + "\n";
+function isLeapYear(year) {
+    if (year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
     }
-
-    alert(result);
 }
 
-var num1 = prompt("Enter start number");
-var num2 = prompt("Enter end number");
-
-sc(num1, num2);
+var year = prompt("Enter a year");
 
 
-
-function ccdd(base, perpendicular) {
-
-    function csqs(num) {
-        return num * num;
-    }
-
-    var hypotenuseSquared =
-        csqs(base) + csqs(perpendicular);
-
-    return Math.sqrt(hypotenuseSquared);
-}
-
-var base = prompt("Enter base");
-var perpendicular = prompt("Enter perpendicular");
-
-alert("Hypotenuse is: " + ccdd(base, perpendicular));
-
-
-function calculateArea(Width,Height) {
-    return Width * Height;
-}
-var area1=calculateArea(10,5)
-alert("Area: " + area1)
-var W = 4;
-var H = 8;
-var area2 = calculateArea(W,H)
-alert("Area: " + area2)
-
-
-function isPalindrome(str) {
-    var reversed = str.split("").reverse().join("");
-    return str === reversed;
-}
-
-var word = prompt("Enter a word to check palindrome:");
-if (isPalindrome(word)) {
-    alert(word + " is a palindrome");
+if (isLeapYear(year)) {
+    alert(year + " is a leap year");
 } else {
-    alert(word + " is not a palindrome");
+    alert(year + " is not a leap year");
 }
 
 
-function capitalizeWords(str) {
-    var words = str.split(" ");
-    for (var i = 0; i < words.length; i++) {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    }
-    return words.join(" ");
+
+function calculateS(a, b, c) {
+    return (a + b + c) / 2;
 }
 
-var text = prompt("Enter a sentence to capitalize:");
-alert("Capitalized: " + capitalizeWords(text));
+function calculateArea(a, b, c) {
+    let S = calculateS(a, b, c);
+    let area = Math.sqrt(S * (S - a) * (S - b) * (S - c));
+    return area;
+}
+let a = 5, b = 6, c = 7;
+console.log("Area of triangle:", calculateArea(a, b, c));
 
 
-function findLongestWord(str) {
-    var words = str.split(" ");
-    var longest = "";
-
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].length > longest.length) {
-            longest = words[i];
-        }
-    }
-    return longest;
+function average(m1, m2, m3) {
+    return (m1 + m2 + m3) / 3;
 }
 
-var sentence = prompt("Enter a sentence to find longest word:");
-alert("Longest word: " + findLongestWord(sentence));
+function percentage(m1, m2, m3) {
+    let totalMarks = m1 + m2 + m3;
+    return (totalMarks / 300) * 100;  // Assuming each subject is out of 100
+}
+
+function mainFunction(m1, m2, m3) {
+    let avg = average(m1, m2, m3);
+    let perc = percentage(m1, m2, m3);
+    console.log("Average:", avg.toFixed(2));
+    console.log("Percentage:", perc.toFixed(2) + "%");
+}
+
+mainFunction(80, 90, 70);
 
 
-function countLetter(str, letter) {
-    var count = 0;
+function customIndexOf(str, char) {
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === letter) {
-            count++;
+        if (str[i] === char) {
+            return i;
         }
     }
-    return count;
+    return -1;
 }
 
-var input1 = prompt("Enter a string:");
-var input2 = prompt("Enter a letter to count:");
-alert("The letter '" + input2 + "' occurs " + countLetter(input1, letterInput) + " times");
+alert("Index of 'l' in 'hello': " + customIndexOf("hello", "l"));
+alert("Index of 'x' in 'hello': " + customIndexOf("hello", "x"));
 
 
 
-
-function gdhn(nhuw) {
-    var njsdb = 2 * Math.PI * nhuw;
-    alert("The circumference is " + njsdb);
+function removeVowels(sentence) {
+    let result = "";
+    for (let i = 0; i < sentence.length; i++) {
+        if (!"aeiouAEIOU".includes(sentence[i])) {
+            result += sentence[i];
+        }
+    }
+    return result;
 }
 
-function bhs(nhuw) {
-    var area = Math.PI * nhuw * nhuw;
-    alert("The area is " + area);
+alert(removeVowels("Hello World"));
+
+
+function toMeters(km) {
+    return km * 1000;
 }
 
-var r = prompt("Enter radius");
+function toFeet(km) {
+    return km * 3280.84;
+}
 
-gdhn(r);
-bhs(r);
+function toInches(km) {
+    return km * 39370.1;
+}
+
+function toCentimeters(km) {
+    return km * 100000;
+}
+
+let km = 5;
+alert("Meters:", toMeters(km));
+alert("Feet:", toFeet(km));
+alert("Inches:", toInches(km));
+alert("Centimeters:", toCentimeters(km));
+
+
+function calculateOvertime(hours) {
+    let overtimeHours = hours > 40 ? hours - 40 : 0;
+    return overtimeHours * 12;
+}
+
+
+
+let hoursWorked = 45;
+alert("Overtime pay:", calculateOvertime(hoursWorked));
+
+function calculateNotes(amount) {
+    let hundreds = Math.floor(amount / 100);
+    let remainder = amount % 100;
+
+    let notes100 = Math.floor(hundreds);
+    let notes50 = Math.floor(remainder / 50);
+    let notes10 = Math.floor((remainder % 50) / 10);
+
+    alert("100 notes:", notes100);
+    alert("50 notes:", notes50);
+   alert("10 notes:", notes10);
+}
+
+
+calculateNotes(780); 
+
+
+function mobileAlert(params) {
+        alert("Thanks for purchasing a phone from us!");
+}
+
+
+function deleteRow(rowNumber) {
+    document.getElementById("studentTable").deleteRow(rowNumber);
+}
+var count = 0;
